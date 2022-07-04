92643b9d9b21d9ef59fff195117b7286233cd69c
Beatriz Férre <93141694+biaferre@users.noreply.github.com>
2022-07-04 16:59:55 -0300

    Initial Commit

---
+++ triunfoProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  triunfoProject
+//
+//  Created by Bof on 04/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
